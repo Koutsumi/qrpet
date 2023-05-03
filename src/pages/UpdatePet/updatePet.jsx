ac7442157db97c45edb324574ad2e9326ac97c4c
Koutsumi <febaccarini@gmail.com>
2023-05-02 21:54:54 -0300

    puxar dados do pet para update

--- src/pages/UpdatePet/updatePet.jsx
+++ src/pages/UpdatePet/updatePet.jsx
@@ -1,4 +1,27 @@
+import { useEffect, useState } from "react"
+import { useParams } from "react-router-dom"
+import { onSnapshot, collection, doc, deleteDoc, serverTimestamp, updateDoc, QuerySnapshot, addDoc} from "firebase/firestore"
+import  db  from '../../services/firebase'
+
 function UpdatePet() {
+
+    const { id } = useParams()
+    const [myPet, setMyPet] = useState()
+
+    const ref_pet = collection(db, "qrpet")
+
+    useEffect(() => {
+
+        const db_pet = doc(ref_pet, id)
+
+        const data_pet = onSnapshot(db_pet, (QuerySnapshot) => {
+            setMyPet({id: QuerySnapshot.id, ...QuerySnapshot.data()})
+        })
+
+    }, [id])
+
+    console.log(myPet)
+
     return(
         <main>
             <section>
