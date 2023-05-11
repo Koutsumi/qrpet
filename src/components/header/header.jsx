50efd9b9b1f2c55a621469526bca2271d3e077ec
flavioBrancalhao <fgbrancalhao@gmail.com>
2023-05-11 20:26:47 -0300

    my pet e inovaçoes

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,15 +1,57 @@
+import { useEffect, useState } from "react"
 import Logo from "../logo/logo"
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+import 'react-toastify/dist/ReactToastify.css';
+import { signOut } from "firebase/auth";
+
+
+
 
 
 function Header() {
 
+    const [logado, setLogado]= useState(false);
+    const auth = getAuth();
     
+
+    const handleLogout = () => {
+        signOut(auth).then(() => {
+          // Sign-out successful.
+          
+          console.log("Signed out successfully")
+        }).catch((error) => {
+          // An error happened.
+        });
+      }
+
+    useEffect(() => {
+        onAuthStateChanged(auth, (user) => {
+          if (user) {
+            // User is signed in, see docs for a list of available properties
+            // https://firebase.google.com/docs/reference/js/firebase.Use
+            setLogado(true)
+            
+            // ...
+          } else {
+            console.log('Não logado')
+            
+            setLogado(false)
+          }
+        });
+    
+        handleLogout 
+    
+      }, []);
     return(
         <header>
             <section className="w-full py-4 lg:py-9 bg-principal flex flex-row items-center justify-center text-[1.1em] lg:h-[7vh]">
                 <div className="w-[90%] direction-row ">
                     <a href="/"><Logo/></a>
-                    <div> <a className="text-white mr-3" href="/login" >Login</a>  <a className="btn-primary" href="/register">Registrar</a></div>
+
+                    <div className={logado ? "hidden" : "block"}> <a className="text-white mr-3" href="/login" >Login</a> <a className="btn-primary" href="/register">Registrar</a></div>
+                    <div className={logado ? "block" : "hidden"}> <a className="text-white mr-3" href="/painel" >Meus pets</a><a className="btn-primary " onClick={handleLogout} href="/" >Sair</a></div>
                 </div>
             </section>
         </header>
--- src/pages/MyPet/myPet.jsx
+++ src/pages/MyPet/myPet.jsx
@@ -1,9 +1,144 @@
+import React from 'react';
+import { signOut } from "firebase/auth";
+import { onSnapshot, collection, doc, deleteDoc, serverTimestamp, updateDoc, QuerySnapshot, addDoc, query, where, getDocs } from "firebase/firestore"
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+import { useNavigate, useParams } from 'react-router-dom';
+import { useEffect, useState } from "react"
+import db from '../../services/firebase'
+import Icone from '../../components/svgs/icone';
+
+
+
 function MyPet() {
+
+   
+    const { id } = useParams();
+    const [myPet, setMyPet] = useState();
+    const [petName, setPetName] = useState();
+    const [details, setDetails] = useState();
+    const [ownerAddress, setOwnerAddress] = useState();
+    const [ownerFirst, setOwnerFirst] = useState();
+    const [ownerSecond, setOwnerSecond] = useState();
+    const [ownerPhone, setOwnerPhone] = useState();
+    const [ownerPhoneSecond, setOwnerPhoneSecond] = useState();
+    const [petBirth, setPetBirth] = useState();
+    const [petGender, setPetGender] = useState();
+    const [petImage, setPetImage] = useState();
+    const [years, setYears] = useState();
+    const ref_pet = collection(db, "qrpet")
+    const [petGederBr, setPetGederBr] = useState();
+
+    async function getPet(){
+        if(myPet != undefined){
+            setPetName(myPet.petName)
+            setDetails(myPet.details)
+            setOwnerAddress(myPet.owner_address)
+            setOwnerFirst(myPet.owner_first)
+            setOwnerSecond(myPet.owner_second)
+            setOwnerPhone(myPet.owner_phone)
+            setOwnerPhoneSecond(myPet.owner_phone_second)
+            setPetBirth(myPet.petBirth)
+            setPetImage(myPet.petImage)
+            return myPet.petBirth;
+
+        }else {
+            console.log("ERROR")
+
+        }
+
+    }
+    
+    async function getGender(){
+        setPetGender(myPet.petGender)
+        return myPet.petGender;
+    }
+    useEffect(() => {
+        
+    
+
+        const db_pet = doc(ref_pet, id)
+
+        const data_pet = onSnapshot(db_pet, (QuerySnapshot) => {
+            setMyPet({id: QuerySnapshot.id, ...QuerySnapshot.data()})
+        })
+
+
+
+    }, [id])
+
+
+    
+
+    useEffect(() => {
+
+        const db_pet = doc(ref_pet, id)
+
+        const data_pet = onSnapshot(db_pet, (QuerySnapshot) => {
+            setMyPet({id: QuerySnapshot.id, ...QuerySnapshot.data()})
+        })
+
+    }, [id])
+
+    useEffect(() => {
+        getPet().then(age => { 
+            const date = new Date();
+            const yearsD = date.getFullYear();
+            
+           setYears( yearsD - parseInt(age));
+
+
+    
+        })
+        getGender().then(gender => { 
+            if(gender == "male"){
+                setPetGederBr("Macho");
+            }else if(gender == "female"){
+                setPetGederBr("Fêmea");
+            }
+        })
+ 
+    }, [myPet])
     return(
         <main>
-            <section>
-                <h1>MyPet</h1>
-            </section>
+    
+                    <section className='w-full flex flex-col items-center justify-center text-lg'>
+                    <img src={petImage} alt="" className='w-[150px] m-auto mt-3 h-[150px] bg-contain shadow-lg rounded-lg border-4 border-principal '/>
+                           <div className="bg-principal flex flex-col w-[90%] mt-4 m-auto rounded-md items-center justify-center text-center text-white p-3">
+                                <h1 className='font-bold text-3x1'>{petName}</h1>
+                                <h1 className='font-bold text-3x1'>{years} Anos</h1>
+                           </div>
+                           <h1 className='mt-10 mb-5 text-principal font-bold text-[1.3em]'>{petGederBr}</h1>
+                           
+                            <div className="w-[80%] flex flex-col justify-center mt-4 ">
+                                <div className="flex mb-2 items-center gap-2 text-[1.1em]"><Icone/><h1 className='text-principal'>Donos:</h1></div>
+                                    <h2 className='ml-10'>{ownerFirst}</h2>
+                                    <h2 className='ml-10 mb-4 '>{ownerSecond} </h2>
+                                    <hr className='border-b-2 border-principal' />
+                           </div>
+                            <div className="w-[80%] flex flex-col justify-center mt-4 ">
+                            <div className="flex mb-2 items-center gap-2 text-[1.1em]"><Icone/><h1 className='text-principal'>Endereço:</h1></div>
+                                <h1 className='ml-10 mb-4' >{ownerAddress}</h1>
+                                <hr className='border-b-2 border-principal' />
+                            </div>
+                            <div className="w-[80%] flex flex-col justify-center mt-4 ">
+                            <div className="flex mb-2 items-center gap-2 text-[1.1em]"><Icone/><h1 className='text-principal'>Contato:</h1></div>
+                                <h1 className='ml-10'>Principal: {ownerPhone}</h1>
+                                <h1 className='ml-10 mb-4 '>Emergenta: {ownerPhoneSecond}</h1>
+                                    <hr className='border-b-2 border-principal' />  
+                            </div>
+                            <div className="w-[80%] flex flex-col justify-center mt-4 ">
+                            <div className="flex mb-2 items-center gap-2 text-[1.1em]"><Icone/><h1 className='text-principal'>Observação:</h1></div>
+
+                            <h1 className='ml-10 mb-4 '>{details}</h1>
+                            </div>
+
+                            
+                    </section>
+               
+                    
+                        
+              
+             
         </main>
     )
 }
