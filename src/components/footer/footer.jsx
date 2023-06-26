6f2c2bb9fc0023f7fe024cb98d7c7f3477938b27
Koutsumi <febaccarini@gmail.com>
2023-06-26 20:53:14 -0300

    enviar email para alterar senha

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,32 +1,57 @@
-import { useEffect, useState } from "react"
-import Logo from "../logo/logo"
+import { useEffect, useState } from "react";
+import Logo from "../logo/logo";
 
 function Footer() {
-    const [year, setYear] = useState()
+  const [year, setYear] = useState();
 
-    useEffect(() => {
-        const today = new Date()
-        const thisYear = today.getFullYear()
-        setYear(thisYear)
-    }, [])
+  useEffect(() => {
+    const today = new Date();
+    const thisYear = today.getFullYear();
+    setYear(thisYear);
+  }, []);
 
-    return(
+  return (
+    <footer>
+      <section className="bg-principal w-full flex flex-col items-center justify-center text-white text-center text-[1.2em] pb-2">
+        <h4 className="w-full my-2"> QrPet </h4>
 
-        <footer>
-            <section className="bg-principal w-full flex flex-col items-center justify-center text-white text-center text-[1.2em] pb-2" >
-               
-                <h4 className="w-full my-2"> QrPet </h4>
-                
-                    <h4 className="w-full m-2">Desenvolvido por</h4>
-                    <div className="w-[900px] lg:flex flex-row">
-                    <div className="w-full m-2  "><h5 > Fernanda </h5>  <a href="https://www.linkedin.com/in/fernanda-matuda-baccarini-455b39171/" target="_blank">Linkedin </a>|<a href="https://github.com/Koutsumi" target="_blank"> Git Hub</a></div><div className="w-full m-2"><h5>Flavio</h5> <a href="https://www.linkedin.com/in/flavio-brancalhao-659574169/" target="_blank">Linkedin </a>|<a href="https://github.com/FlavioBrancalhao" target="_blank"> Git Hub</a></div>
-                    </div>
-                
-            </section>
-            <h6 className="flex items-center justify-center">©Ntec soluções {year}</h6>
-            
-        </footer>
-    )
+        <h4 className="w-full my-2">Desenvolvido por</h4>
+        <div className="w-[90%] lg:flex flex-row">
+          <div className="w-full my-2  ">
+            <h5> Fernanda </h5>{" "}
+            <a
+              href="https://www.linkedin.com/in/fernanda-matuda-baccarini-455b39171/"
+              target="_blank"
+            >
+              Linkedin{" "}
+            </a>
+            |
+            <a href="https://github.com/Koutsumi" target="_blank">
+              {" "}
+              Git Hub
+            </a>
+          </div>
+          <div className="w-full my-2">
+            <h5>Flavio</h5>{" "}
+            <a
+              href="https://www.linkedin.com/in/flavio-brancalhao-659574169/"
+              target="_blank"
+            >
+              Linkedin{" "}
+            </a>
+            |
+            <a href="https://github.com/FlavioBrancalhao" target="_blank">
+              {" "}
+              Git Hub
+            </a>
+          </div>
+        </div>
+      </section>
+      <h6 className="flex items-center justify-center">
+        ©Ntec soluções {year}
+      </h6>
+    </footer>
+  );
 }
 
-export default Footer
+export default Footer;
--- src/pages/ForgotPass/forgotPass.jsx
+++ src/pages/ForgotPass/forgotPass.jsx
@@ -1,9 +1,62 @@
-function forgotPass(){
-    return (
-        <section>
+import React from 'react';
+import firebase from '../../services/firebase';
+import { useState } from 'react';
+import { getAuth, sendPasswordResetEmail } from "firebase/auth";
 
-        </section>
-    )
+function ForgotPass() {
+
+  const [email, setEmail] = useState('');
+  const [modal, setModal] = useState(false);
+
+  const auth = getAuth(); 
+
+  const triggerResetEmail = async (e) => {
+    e.preventDefault();
+    await sendPasswordResetEmail(auth, email).then((res) =>{
+      console.log("Password reset email sent")
+      setModal(true)
+    }).catch(err => {
+      console.log(err)
+    })
+    
+  }
+
+  return (
+    <main>
+      <section className="direction-col w-full my-4">
+        <div className="direction-col w-[90%] min-h-[60vh]">
+          <h1 className="text-center my-4 uppercase text-terciaria font-bold tracking-[5px] text-[1.5em]">
+            Esqueci minha senha
+          </h1>
+
+          <form onSubmit={triggerResetEmail} className="flex flex-col w-[275px]">
+            <label htmlFor="email">E-mail cadastrado no sistema</label>
+            <input
+              className="inp-primary"
+              required
+              type="email"
+              name="email"
+              id="email"
+              onChange={(e) => {
+                setEmail(e.target.value);
+              }}
+            />
+            <button className="btn-primary">Recuperar acesso</button>
+          </form>
+
+          <a href="/login">Voltar</a>
+        </div>
+      </section>
+
+      <section className={modal ? "fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 sm:translate-x-1/5 sm:translate-y-1/5": "hidden"}>
+        <div className='direction-col w-350 h-250 bg-secundaria rounded-lg text-center p-4'>
+              <p>Um e-mail foi enviado para {email}</p>
+              <p className='mb-4'>Acesse o link fornecido para redefinir sua senha</p>
+              <button className="btn-primary"><a href="/login">OK</a></button>
+        </div>
+      </section>
+    </main>
+  );
 }
 
-export default forgotPass
+export default ForgotPass;
